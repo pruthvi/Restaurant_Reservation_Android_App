168d50163214cd9a34f54a3453d2d1eebdf60931
Jiabin <jiabintang@hotmail.com>
2018-12-11 17:04:28 -0500

    Fixed reservation

--- RestaurantReservation/app/src/main/java/com/restaurantreservation/LoginActivity.java
+++ RestaurantReservation/app/src/main/java/com/restaurantreservation/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
         dbManager = new DatabaseManager(this);
 
-        //dbManager.deleteDatabase(this);
+   //     dbManager.deleteDatabase(this);
 
         dbManager.CreateDatabase(getApplicationContext());
         dbManager.InitDatabase(tables, tableCreatorString);
