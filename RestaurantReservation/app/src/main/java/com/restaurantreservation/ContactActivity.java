10b64aecad3617ceb29e112fb0b06894b8839cc0
Pruthvi <pruthvi@users.noreply.github.com>
2018-12-11 16:05:50 -0500

    Updated contact activity

--- RestaurantReservation/app/src/main/java/com/restaurantreservation/ContactActivity.java
+++ RestaurantReservation/app/src/main/java/com/restaurantreservation/ContactActivity.java
@@ -6,10 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-public class ContactActivity extends AppCompatActivity {
+
+public class ContactActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     MapView mapView;
     GoogleMap map;
@@ -18,6 +29,21 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+
+
+        Bundle mapViewBundle = null;
+
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+
+
+
+
+
     }
 
     public  void onClickCall(View view)
@@ -25,8 +51,55 @@ public class ContactActivity extends AppCompatActivity {
         Intent callIntent = new Intent(Intent.ACTION_CALL);
         callIntent.setData(Uri.parse("tel:4377769707"));
 
-
         startActivity(callIntent);
 
     }
+
+    public  void onClickEmail(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","email@email.com", null));
+
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng position = new LatLng(43.785069, -79.228174);
+
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.addMarker(new MarkerOptions().position(position).title("Centennial College"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position,15));
+    }
+
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
